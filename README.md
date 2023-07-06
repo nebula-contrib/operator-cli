8f1975c6cf393416c35775fb890341a34f92434c
sixwaaaay <lw.2675@qq.com>
2023-07-06 23:52:01 +0800

    feat: studio command

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # operator-cli
+
+A command line interface to facilitate the use of the nebula graph.
+
+## Installation
+
+```bash
+go install github.com/nebula-contrib/operator-cli@latest
+```
---
+++ internal/util/util.go
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2023 The nebula-contrib Authors.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package util
+
+import (
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func NewClientSet(kubeconfig string) (*kubernetes.Clientset, error) {
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		return nil, err
+	}
+	clientSet, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	return clientSet, nil
+}
